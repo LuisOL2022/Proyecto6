72229415aceb08b34d6e4152d05b4c9e887cd19a
Luis OLivares <luisolivares.r717@gmail.com>
2023-08-04 13:14:16 -0400

    cambio ruta imagen

--- src/assets/components/Card/Card.jsx
+++ src/assets/components/Card/Card.jsx
@@ -7,7 +7,7 @@ const CardComponent = () => {
         <div className="row">
         <div className="col-md-3">
             <Card>
-            <Card.Img variant="top" src="src/assets/img/isibaby.png" />
+            <Card.Img variant="top" src="isibaby.png" />
             <Card.Body>
                 <Card.Title>ISSIBABY</Card.Title>
                 <Card.Text>
